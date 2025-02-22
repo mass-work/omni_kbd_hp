15d856e60f3b045b172edb8f1ccd43e4faf299ce
masatokondo <masatokondou0114@gmail.com>
2025-02-22 18:27:07 +0900

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './GlobalStyle';
 import App from './App';
 
+const basename = import.meta.env.BASE_URL || '/';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <GlobalStyle />
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>

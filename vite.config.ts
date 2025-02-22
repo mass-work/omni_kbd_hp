58712e54e2cad9e2afd3bbe8ae1bf70b4f2682f1
masatokondo <masatokondou0114@gmail.com>
2025-02-22 16:35:07 +0900

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   // GitHub Pagesで公開する場合はbaseを指定（例: https://<ユーザー名>.github.io/omni-kbd/）
-  // base: '/omni-kbd-hp/',
+  base: '/omni-kbd-hp/',
   plugins: [react()],
   server: {
     open: true, // サーバー起動時にブラウザを開きたい場合

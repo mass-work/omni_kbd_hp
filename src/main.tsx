fdcd28abef7b3beb3b1076fbc1ca09ad87cba27f
masatokondo <masatokondou0114@gmail.com>
2025-02-22 18:44:50 +0900

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import App from './App';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <GlobalStyle />
-    <BrowserRouter>
+    <BrowserRouter basename="/omni_kbd_hp/">
       <App />
     </BrowserRouter>
   </React.StrictMode>

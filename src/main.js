497e653be295f97a35da2ce17735864562b9f154
masatokondo <masatokondou0114@gmail.com>
2025-02-22 18:13:34 +0900

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './GlobalStyle';
 import App from './App';
-ReactDOM.createRoot(document.getElementById('root')).render(_jsxs(React.StrictMode, { children: [_jsx(GlobalStyle, {}), _jsx(HashRouter, { basename: "/omni_kbd_hp/", children: _jsx(App, {}) })] }));
+ReactDOM.createRoot(document.getElementById('root')).render(_jsxs(React.StrictMode, { children: [_jsx(GlobalStyle, {}), _jsx(BrowserRouter, { children: _jsx(App, {}) })] }));

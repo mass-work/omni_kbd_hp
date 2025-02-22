cbe9f8fd83718b452a03122ba325818ee8731fce
masatokondo <masatokondou0114@gmail.com>
2025-02-22 17:51:49 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import styled from 'styled-components';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import BuildGuide from './pages/BuildGuide';
 function App() {
-    return (_jsxs(Container, { children: [_jsx(Header, {}), _jsx(MainContent, { children: _jsx(HashRouter, { basename: "/omni_kbd_hp/", children: _jsxs(Routes, { children: [_jsx(Route, { path: "/", element: _jsx(Home, {}) }), _jsx(Route, { path: "/buildguide", element: _jsx(BuildGuide, {}) }), _jsx(Route, { path: "/about", element: _jsx(About, {}) })] }) }) }), _jsx(Footer, {})] }));
+    return (_jsxs(Container, { children: [_jsx(Header, {}), _jsx(MainContent, { children: _jsxs(Routes, { children: [_jsx(Route, { path: "/", element: _jsx(Home, {}) }), _jsx(Route, { path: "/buildguide", element: _jsx(BuildGuide, {}) }), _jsx(Route, { path: "/about", element: _jsx(About, {}) })] }) }), _jsx(Footer, {})] }));
 }
 export default App;
 const Container = styled.div `
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { , Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
